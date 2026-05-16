7665b78291a4cbb24021b1ba7d3daf09700fb831
Jongmin Kim <vwmartin@naver.com>
2026-05-16 23:33:56 +0900

    feat: VectorExtension

---
+++ Extensions/VectorExtension.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace JxModule
+{
+    public static class VectorExtension
+    {
+        public static Vector3 GetDirection(this Vector3 from, Vector3 to)
+        {
+            return (to - from).normalized;
+        }
+
+        public static Vector2 GetDirection2D(this Vector3 from, Vector3 to)
+        {
+            var direction = to - from;
+            return new Vector3(direction.x, direction.y).normalized;
+        }
+
+        public static Vector2 GetDirection2D(this Vector2 from, Vector2 to)
+        {
+            return (to - from).normalized;
+        }
+    }
+}
